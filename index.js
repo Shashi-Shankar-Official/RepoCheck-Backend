f4648efeaea4c6c75dfc1d60d3e3146fac3cade4
Shashi Shankar <shankarshashi386@gmail.com>
2025-10-24 03:19:26 +0530

    cors error corrected

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const app = express();
 
 // Enable CORS for the frontend
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: [
+        'http://localhost:5173',
+        process.env.FRONTEND_URL
+    ].filter(Boolean),
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type']
 }));
